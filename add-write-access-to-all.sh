bb82b7f39ceb6e5c235730d812e4b15d07c42da3
nachoparker <nacho@ownyourbits.com>
2018-02-07 20:20:35 +0100

    fixes for names with spaces and special characters

--- add-write-access-to-all.sh
+++ add-write-access-to-all.sh
@@ -1,28 +1,16 @@
 #!/bin/bash
-# Ask the PATH
-#echo Geef het volledige Linuxpad naar de map, waar je schrijfrechten voor wilt geven 
-#varname_
-#gtk_window_set_transient_for()
-inputStr=$(zenity --entry --title=”Set Direcory” --text=”Enter full PATH”) 
-#
-echo "Making Directory $inputStr writable to all .....You may be asked for your password."
-chmod a+w -R $inputStr
-echo  Directory $inputStr is now writable
-exit
 
-# License
-#
-# This script is free software; you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This script is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this script; if not, write to the
-# Free Software Foundation, Inc., 59 Temple Place, Suite 330,
-# Boston, MA  02111-1307  USA
+DIR="$( zenity --file-selection --directory 2>/dev/null )"
+
+FILES=()
+
+while IFS=  read -r -d $'\0'; do
+    FILES+=("$REPLY")
+done < <( find "$DIR" -not -writable -type f -print0 )
+
+[[ ${#FILES[0]} -eq 0 ]] && { echo "All files have write permissions"; exit 0; }
+
+for f in "${FILES[@]}"; do
+  echo -e "Fixing writing permissions for file \e[1m$f\e[0m"
+  chmod +w "$f"
+done
