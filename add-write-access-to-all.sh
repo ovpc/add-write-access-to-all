244f925e24a44dbe0d739dab6b055ad2b47ec1c4
Oliver V <ovpc@users.noreply.github.com>
2018-02-07 10:11:19 +0100

    Added zenity to script
    Using zenity for a pop-up window to ask for PATH

--- add-write-access-to-all.sh
+++ add-write-access-to-all.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 # Ask the PATH
-echo Type the full PATH to folder needing write access 
-read varname
-echo The folders name is $varname asigning write access.....You may be asked your (sudo) password.
-chmod a+w -R $varname
-echo Write access granted to $varname 
+#echo Geef het volledige Linuxpad naar de map, waar je schrijfrechten voor wilt geven 
+#varname_
+#gtk_window_set_transient_for()
+inputStr=$(zenity --entry --title=”Set Direcory” --text=”Enter full PATH”) 
+#
+echo "Making Directory $inputStr writable to all .....You may be asked for your password."
+chmod a+w -R $inputStr
+echo schrijfrechten van $inputStr aangepast
 exit
+
+# License
+#
+# This script is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This script is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this script; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+# Boston, MA  02111-1307  USA
