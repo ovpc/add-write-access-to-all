94002ccf5225b4d3fbf9519c6273714e54e7612c
nacho <nacho.nunez@aoifes.com>
2018-02-08 09:02:40 +0100

    fix permissions for all users, with sudo powers

--- add-write-access-to-all.sh
+++ add-write-access-to-all.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+type gksudo &>/dev/null || { zenity --info --text "Please, install gksudo"; exit 0; }
+
 DIR="$( zenity --file-selection --directory 2>/dev/null )"
 
 FILES=()
@@ -8,9 +10,9 @@ while IFS=  read -r -d $'\0'; do
     FILES+=("$REPLY")
 done < <( find "$DIR" -not -writable -type f -print0 )
 
-[[ ${#FILES[0]} -eq 0 ]] && { echo "All files have write permissions"; exit 0; }
+[[ ${#FILES[0]} -eq 0 ]] && { zenity --info --text "All files have write permissions"; exit 0; }
 
 for f in "${FILES[@]}"; do
   echo -e "Fixing writing permissions for file \e[1m$f\e[0m"
-  chmod +w "$f"
+  gksudo chmod ugo+w "$f"
 done
