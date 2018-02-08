74bba301ac4b472a82241dc83f8a6b94b8c5dced
nacho <nacho.nunez@aoifes.com>
2018-02-08 11:26:07 +0100

    only ask for password once

--- add-write-access-to-all.sh
+++ add-write-access-to-all.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-type gksudo &>/dev/null || { zenity --info --text "Please, install gksudo"; exit 0; }
-
+gksudo -lk -- bash -c '
 DIR="$( zenity --file-selection --directory 2>/dev/null )"
 
 FILES=()
 
-while IFS=  read -r -d $'\0'; do
+while IFS=  read -r -d $'\''\0'\''; do
     FILES+=("$REPLY")
 done < <( find "$DIR" -type f ! -perm -ugo+w -print0 )
 
@@ -14,5 +13,6 @@ done < <( find "$DIR" -type f ! -perm -ugo+w -print0 )
 
 for f in "${FILES[@]}"; do
   echo -e "Fixing writing permissions for file \e[1m$f\e[0m"
-  gksudo chmod ugo+w "$f"
+  chmod ugo+w "$f"
 done
+'
